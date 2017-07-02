bf470a0919dbef4afdb04b6549490d16778d9f84
Eddykasp <maxkasperowski@gmail.com>
2017-07-02 18:04:40 +0800

    add block and platform constructors

---
+++ js/block.js
@@ -0,0 +1,10 @@
+var Block = function (x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.draw = function () {};
+    this.c = 'white';
+};
+
+module.exports = Block;
