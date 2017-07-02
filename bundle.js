d7054843f5dedb79a52e51f4f6110ba66fcc8380
Eddykasp <maxkasperowski@gmail.com>
2017-07-02 11:16:49 +0800

    added falling sprite
    changed standing and jumping sprite

--- bundle.js
+++ bundle.js
@@ -487,7 +487,10 @@ var Person = function(px, py){
     this.sprites = [
         function (ctx) {
             ctx.fillStyle = this.c;
-            ctx.fillRect(this.px - 5, this.py - 20, 10, 20);
+            ctx.fillRect(this.px - 5, this.py - 17, 10, 17);
+            ctx.fillRect(this.px - 4, this.py - 18, 8, 1);
+            ctx.fillRect(this.px - 3, this.py - 19, 6, 1);
+            ctx.fillRect(this.px - 2, this.py - 20, 4, 1);
             ctx.fillStyle = 'black';
             if (this.xv > 0.05){
                 ctx.fillRect(this.px + 1, this.py - 17, 2, 3);
@@ -500,8 +503,29 @@ var Person = function(px, py){
         },
         function (ctx) {
             ctx.fillStyle = this.c;
-            ctx.fillRect(this.px - 5,this.py - 17, 10, 17);
-            ctx.fillRect(this.px - 7, this.py - 5, 14, 5);
+            ctx.fillRect(this.px - 5, this.py - 14, 10, 14);
+            ctx.fillRect(this.px - 4, this.py - 15, 8, 1);
+            ctx.fillRect(this.px - 3, this.py - 16, 6, 1);
+            ctx.fillRect(this.px - 2, this.py - 17, 4, 1);
+            ctx.fillRect(this.px - 6, this.py - 7, 12, 7);
+            ctx.fillStyle = 'black';
+            if(this.xv > 0.05){
+                ctx.fillRect(this.px + 1, this.py - 15, 2, 3);
+            } else if(this.xv < -0.05){
+                ctx.fillRect(this.px - 3, this.py - 15, 2, 3);
+            } else {
+                ctx.fillRect(this.px + 1, this.py - 15, 2, 3);
+                ctx.fillRect(this.px - 3, this.py - 15, 2, 3);
+            }
+        },
+        function (ctx) {
+            ctx.fillStyle = this.c;
+            ctx.fillRect(this.px - 5, this.py - 13, 10, 13);
+            ctx.fillRect(this.px - 4, this.py - 14, 8, 1);
+            ctx.fillRect(this.px - 3, this.py - 15, 6, 1);
+            ctx.fillRect(this.px - 2, this.py - 16, 4, 1);
+            ctx.fillRect(this.px - 6, this.py - 10, 12, 9);
+            ctx.fillRect(this.px - 7, this.py - 8, 14, 7);
             ctx.fillStyle = 'black';
             if(this.xv > 0.05){
                 ctx.fillRect(this.px + 1, this.py - 15, 2, 3);
@@ -532,6 +556,9 @@ var Person = function(px, py){
             this.draw = this.sprites[1];
         } else {
             this.draw = this.sprites[0];
+        }
+        if(this.yv > 0.2){
+            this.draw = this.sprites[2];
         }
     };
 };
