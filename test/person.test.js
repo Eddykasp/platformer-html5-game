fa0937a80f6ee2e7fb37649f0351493679a1ae7b
Eddykasp <maxkasperowski@gmail.com>
2017-07-05 09:22:05 +0800

    add messages to tests

--- test/person.test.js
+++ test/person.test.js
@@ -3,23 +3,23 @@ var Person = require('../js/person');
 
 test('person constructor', t => {
     var p = new Person(0, 0);
-    t.equal(p.px, 0);
-    t.equal(p.py, 0);
-    t.equal(p.xv, 0);
-    t.equal(p.yv, 0);
-    t.equal(p.onG, false);
-    t.equal(p.c, '#ffffff');
+    t.equal(p.px, 0, 'x position');
+    t.equal(p.py, 0, 'y position');
+    t.equal(p.xv, 0, 'x velocity');
+    t.equal(p.yv, 0, 'y velocity');
+    t.equal(p.onG, false, 'is on ground');
+    t.equal(p.c, '#ffffff', 'colour is default white');
     t.end();
 });
 
 test('person move', t => {
     var p = new Person(0, 0);
-    t.equal(p.px, 0);
-    t.equal(p.py, 0);
+    t.equal(p.px, 0, 'x position before moving');
+    t.equal(p.py, 0, 'y position before moving');
     p.xv = 10;
     p.yv = 10;
     p.move();
-    t.equal(p.px, 10);
-    t.equal(p.py, 10);
+    t.equal(p.px, 10, 'x position after moving');
+    t.equal(p.py, 10, 'y position after moving');
     t.end();
 });
