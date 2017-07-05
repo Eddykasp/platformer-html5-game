86fe3ca540ce18c63d0aa2d8b4f8d33f47e5782a
Eddykasp <maxkasperowski@gmail.com>
2017-07-05 12:30:07 +0800

    add more move test cases

--- test/person.test.js
+++ test/person.test.js
@@ -12,7 +12,7 @@ test('person constructor', t => {
     t.end();
 });
 
-test('person move', t => {
+test('person move positive x / positive y', t => {
     var p = new Person(0, 0);
     t.equal(p.px, 0, 'x position before moving');
     t.equal(p.py, 0, 'y position before moving');
@@ -21,5 +21,41 @@ test('person move', t => {
     p.move();
     t.equal(p.px, 10, 'x position after moving');
     t.equal(p.py, 10, 'y position after moving');
+    t.end();
+});
+
+test('person move positive x / negative y', t => {
+    var p = new Person(0, 0);
+    t.equal(p.px, 0, 'x position before moving');
+    t.equal(p.py, 0, 'y position before moving');
+    p.xv = 10;
+    p.yv = -10;
+    p.move();
+    t.equal(p.px, 10, 'x position after moving');
+    t.equal(p.py, -10, 'y position after moving');
+    t.end();
+});
+
+test('person move negative x / positive y', t => {
+    var p = new Person(0, 0);
+    t.equal(p.px, 0, 'x position before moving');
+    t.equal(p.py, 0, 'y position before moving');
+    p.xv = -10;
+    p.yv = 10;
+    p.move();
+    t.equal(p.px, -10, 'x position after moving');
+    t.equal(p.py, 10, 'y position after moving');
+    t.end();
+});
+
+test('person move negative x / negative y', t => {
+    var p = new Person(0, 0);
+    t.equal(p.px, 0, 'x position before moving');
+    t.equal(p.py, 0, 'y position before moving');
+    p.xv = -10;
+    p.yv = -10;
+    p.move();
+    t.equal(p.px, -10, 'x position after moving');
+    t.equal(p.py, -10, 'y position after moving');
     t.end();
 });
