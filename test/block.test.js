86807efd1104b7e7c6665a08ea3438e861cc75c4
Eddykasp <maxkasperowski@gmail.com>
2017-07-06 22:58:59 +0800

    add point as structure object

--- test/block.test.js
+++ test/block.test.js
@@ -1,14 +1,15 @@
 var test = require('tape');
 var Block = require('../js/block');
+var Point = require('../js/point');
 
 test('point is inside method', t => {
     var block = new Block(30, 30);
     //set position to (0, 0)
     block.x = 0;
     block.y = 0;
-    t.equal(block.pointIsInside({x: 5, y: 5}), true, 'point is inside block');
-    t.equal(block.pointIsInside({x: -1, y: 5}), false, 'x outside, y inside > point outside');
-    t.equal(block.pointIsInside({x: 5, y: -1}), false, 'x inside, y outside > point outside');
-    t.equal(block.pointIsInside({x: -1, y: -1}), false, 'x outside, y outside > point outside');
+    t.equal(block.pointIsInside(new Point(5, 5)), true, 'point is inside block');
+    t.equal(block.pointIsInside(new Point(-1, 5)), false, 'x outside, y inside > point outside');
+    t.equal(block.pointIsInside(new Point(5, -1)), false, 'x inside, y outside > point outside');
+    t.equal(block.pointIsInside(new Point(-1, -1)), false, 'x outside, y outside > point outside');
     t.end();
 });
